77a48deec2400041819c95ba665fd85b99015a77
truongngv2003 <truongnguyenmailbka@gmail.com>
2024-06-17 21:59:53 +0700

    Sửa giao diện

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-    role: {type: String, default: 'user'},  // admin, user, manager
+    role: {type: String, default: 'user'},  // admin, user
     name: String,
     password: String,
-    avatar: {type: String, default: 'https://scontent-hkg1-1.xx.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?stp=cp0_dst-png_p40x40&_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH1381gwedxTXsiC0Xeo-peso2H55p0AlGyjYfnmnQCUZMMj6EYcOio77m1_ChBpA-PE96D0YrlnrqfxkZsoNJ7&_nc_ohc=C0FEU6wOyRwQ7kNvgH2sHTS&_nc_ht=scontent-hkg1-1.xx&oh=00_AfBOJFDY-DObJDdTjF-QOQN-bF8QsZ3bgayRykaMFJNcRA&oe=665EBA78'},
+    avatar: {type: String, default: '/image/avatarDefault.svg'},
     totalPost: {type: Number, default: 0},
     pendingPost: {type: Number, default: 0},
     approvedPost: {type: Number, default: 0}, 
